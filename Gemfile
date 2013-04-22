f088874d13fc556b9fdfac64f0dad9207954312e
Robert Fletcher <lobatifricha@gmail.com>
2013-04-21 18:36:22 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'bcrypt-ruby'
+gem 'jquery-rails'
+gem 'pg'
+
+group :assets do
+  gem 'coffee-rails'
+  gem 'sass-rails'
+  gem 'uglifier'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MutantRails::Application.config.secret_token = '6b6744a6ad18d46a8f6770f6605b18a8651d79e72b01d09549a132332d171dd489f25786a69980d04b812b8340b3f174df58efe9f202355ffa80dad70a672c46'
